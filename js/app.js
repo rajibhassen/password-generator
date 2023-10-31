6181a6fcd01d21562ba2e4206c85f5ecf6108fdd
Rajib-hassen <123587348+rajibhassen@users.noreply.github.com>
2023-10-31 00:43:24 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerSet = "abcdefghijklmnopqrstuvwxyz"
+const numberSet = "1234567890"
+const symbolSet = "~!@#$%^&*()_+/"
+
+// selectors
+const passBox = document.getElementById("pass-box")
+const totalChar = document.getElementById("total-char")
+const upperInput = document.getElementById("upper-case")
+const lowerInput = document.getElementById("lower-case")
+const numberInput = document.getElementById("numbers")
+const symbolInput = document.getElementById("symbols")
+const copyText = document.getElementById("copy-btn")
+
+
+
+const getRandomData = (dataSet) => {
+    return dataSet[Math.floor(Math.random() * dataSet.length)]
+}
+
+const generatePassword = (password = "") => {
+    if (upperInput.checked) {
+        password += getRandomData(upperSet)
+    }
+    if (lowerInput.checked) {
+        password += getRandomData(lowerSet)
+    }
+    if (numberInput.checked) {
+        password += getRandomData(numberSet)
+    }
+    if (symbolInput.checked) {
+        password += getRandomData(symbolSet)
+    }
+    if (password.length < totalChar.value) {
+        return generatePassword(password)
+    }
+
+    passBox.innerText = truncateString(password, totalChar.value);
+}
+
+
+generatePassword();
+
+document.getElementById("btn").addEventListener(
+    "click",
+    function() {
+        generatePassword();
+    }
+
+)
+
+
+function truncateString(str, num) {
+    if (str.length > num) {
+        let subStr = str.substring(0, num);
+        return subStr;
+    } else {
+        return str;
+    }
+}
+
+var copyTextareaBtn = document.querySelector('#copy-btn');
+
+copyTextareaBtn.addEventListener('click', function(event) {
+  var copyTextarea = document.querySelector('#pass-box');
+  copyTextarea.focus();
+  copyTextarea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+  } catch (err) {
+    alert('Oops, unable to copy');
+  }
+});
